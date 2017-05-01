d8d904fbe1db6856c26a4ab939ce82df8bba9ca8
marcbruni <marc.bruni@gmx.ch>
2017-05-01 14:44:01 +0200

    eventhandler on searchbutton (keyboard)

--- app/src/main/java/ch/bbcag/youtubeextension/MainActivity.java
+++ app/src/main/java/ch/bbcag/youtubeextension/MainActivity.java
@@ -1,13 +1,48 @@
 package ch.bbcag.youtubeextension;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    sendMessage();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
     }
+
+    public  void sendMessage(){
+        Intent i = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.vogella.com")
+        );
+
+        startActivity(i);
+    }
+
 }
+
