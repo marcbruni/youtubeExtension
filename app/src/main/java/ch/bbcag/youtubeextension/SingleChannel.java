afa4f9498d59296d52518eca27481700fa52ccf4
marcbruni <marc.bruni@gmx.ch>
2017-05-08 10:34:59 +0200

    displays id on SingleChannel View

--- app/src/main/java/ch/bbcag/youtubeextension/SingleChannel.java
+++ app/src/main/java/ch/bbcag/youtubeextension/SingleChannel.java
@@ -3,6 +3,7 @@ package ch.bbcag.youtubeextension;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -17,6 +18,12 @@ public class SingleChannel extends Activity {
 
 
         String id = getIntent().getStringExtra("id");
+
+        setContentView(R.layout.single_channel);
+
+
+        final EditText editText2 = (EditText) findViewById(R.id.editText2);
+        editText2.setText(id.toString());
 
         Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
     }
